0b16b72c412bd5806834b9047eb0376ac07aee23
dyau172 <47676379+dyau172@users.noreply.github.com>
2020-10-25 22:04:07 +0000

    Health cannot go below 0

--- Character.cs
+++ Character.cs
@@ -1,40 +1,42 @@
 using System;
+using System.Collections.Generic;
 
 namespace Character {
     class Character {
         public string Name { get; set; }
-        public int Health { get; set; }
+
+        int health = 100;
+        public int Health {
+            get{return health;}
+            set {
+                if (value <= 0) {
+                    health = 0;
+                }else {
+                    health = value;
+                }
+
+            }
+        }
         public int Defence { get; set; }
 
-        public int Attack {get; set;}
+        public int Attack { get; set; }
 
-        
+        public Character () {
 
-        
-
-        
-
-        public Character(){
-           
-            
         }
 
-        public void Damage ()
-        {
-            int damage = new Random().Next(5,30);
+        public void Damage () {
+            int damage = new Random ().Next (5, 30);
             Health -= damage;
         }
 
-
        
 
+        public void DisplayStats () {
+            Console.WriteLine ($"Name: {Name}");
+            Console.WriteLine ($"Health Points: {Health}");
+            
 
-        public void DisplayStats(){
-            Console.WriteLine($"Name: {Name}");
-            Console.WriteLine($"Health Points: {Health}");
-            Console.WriteLine();
-            
-            
         }
 
     }
